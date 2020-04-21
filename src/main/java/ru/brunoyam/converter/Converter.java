2232683ad41a7d8a6c650a348833b4e58e957dcb
kolosov <spikecyrill@gmail.com>
2020-04-21 23:48:30 +0300

    initial commit

---
+++ src/main/java/ru/brunoyam/converter/Converter.java
@@ -0,0 +1,11 @@
+package ru.brunoyam.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Converter {
+    public static void main(String[] args) {
+        SpringApplication.run(Converter.class);
+    }
+}
